2363bf2bca60592ef076b87011799e4d77583af6
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-11-10 11:52:13 +0100

    work in progress alert

--- src/components/intro-section/intro.jsx
+++ src/components/intro-section/intro.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "./intro.css"
+import "./intro.css";
 
 function intro() {
   return (
     <div className="intro">
+       <div class="alert alert-danger text-uppercase font-weight-bold text-center m-0" role="alert">
+          Work in progress
+          </div>
       <div className="jumbotron jumbotron-fluid h-100 d-flex align-items-center">
         <div className="container text-center">
           <h1 className="display-4">Hi, i'm Adrien</h1>
           <p className="lead">Developer Web FullStack</p>
-          <h2 className="text-uppercase font-weight-bold underline"><u>Work in progress</u></h2>
+         
         </div>
       </div>
     </div>
