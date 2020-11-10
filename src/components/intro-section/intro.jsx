e73717915f246bcb81b079ec600ce5dc0ed8bba3
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-11-10 11:46:50 +0100

    work in progress message

--- src/components/intro-section/intro.jsx
+++ src/components/intro-section/intro.jsx
@@ -8,6 +8,7 @@ function intro() {
         <div className="container text-center">
           <h1 className="display-4">Hi, i'm Adrien</h1>
           <p className="lead">Developer Web FullStack</p>
+          <h2 className="text-uppercase font-weight-bold underline"><u>Work in progress</u></h2>
         </div>
       </div>
     </div>
