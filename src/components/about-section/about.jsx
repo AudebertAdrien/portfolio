744f1c392691e8042e3490ef24480fc6f8dccbc6
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-11-23 17:30:58 +0100

    remove useless image and fix design

--- src/components/about-section/about.jsx
+++ src/components/about-section/about.jsx
@@ -4,7 +4,7 @@ import "./about.css";
 
 function about() {
   return (
-    <div className="container about-card card mt-3 border-0">
+    <div className="container card mt-3 border-0">
       <h3 className="text-center p-5">A LITTLE BIT ABOUT ME</h3>
       <div className="row no-gutters">
         <div className="circular-landscape order-2 m-auto">
@@ -12,7 +12,7 @@ function about() {
         </div>
         <div className="col-md-8 order-1 d-flex align-items-center m-auto">
           <div className="card-body">
-            <p className="card-text">
+            <p className="card-text text-center text-lg-left">
               On the lookout for new technological practices, I take advantage
               of any experiences to deepen my knowledge and technical skills in
               web development. I like to evolve in an ambitious, challenging and
